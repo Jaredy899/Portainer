d83b05799ffbf06cc3979336fd7fa06047ae77fb
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-01-14 08:54:16 -0500

    Update Portainer.sh

--- Portainer.sh
+++ Portainer.sh
@@ -26,6 +26,8 @@ sleep 5
 # Portainer
 docker run -d -p 8000:8000 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
+usermod -aG sudo jared
+
 # nala
 apt install nala
 
